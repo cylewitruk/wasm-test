13025371009729319e9f4426489eb48a33ed3f50
Cyle Witruk <cyle.witruk@outlook.com>
2023-09-27 05:49:26 +0200

    better version, but slower..

--- crates/wasm-test/benches/stack.rs
+++ crates/wasm-test/benches/stack.rs
@@ -6,7 +6,11 @@ use wasm_test::runtime::stack::*;
 criterion_group!{
     name = stack_benches;
     config = Criterion::default().measurement_time(Duration::from_secs(5));
-    targets = local_push
+    targets = 
+        local_push, 
+        exec_single_frame,
+        push_single_local_from_frame,
+        push_five_locals_from_frame
 }
 
 criterion_main!(
@@ -16,6 +20,7 @@ criterion_main!(
 pub fn local_push(c: &mut Criterion) {
     c.bench_function("stack/push/i128", move |b| {
         let stack = Stack::new();
+        let frame = Frame::new(1, &stack, None);
 
         b.iter_batched(
             || {
@@ -25,6 +30,59 @@ pub fn local_push(c: &mut Criterion) {
                 stack.local_push(Value::Int(5));
             },
             criterion::BatchSize::SmallInput,
+        );
+    });
+}
+
+pub fn exec_single_frame(c: &mut Criterion) {
+    c.bench_function("stack/exec-single-frame", move |b| {
+        let stack = Stack::new();
+
+        b.iter_batched(|| {
+
+            },
+            |_| {
+                stack.exec(|_| { })
+            }, 
+            criterion::BatchSize::SmallInput
+        );
+    });
+}
+
+pub fn push_single_local_from_frame(c: &mut Criterion) {
+    c.bench_function("stack/push_from_frame", move |b| {
+        let stack = Stack::new();
+
+        b.iter_batched(|| {
+                stack.clear_locals();
+            },
+            |_| {
+                stack.exec(|frame| {
+                    frame.local_push(Value::Int(5));
+                });
+            }, 
+            criterion::BatchSize::SmallInput
+        );
+    });
+}
+
+pub fn push_five_locals_from_frame(c: &mut Criterion) {
+    c.bench_function("stack/push_from_frame", move |b| {
+        let stack = Stack::new();
+
+        b.iter_batched(|| {
+                stack.clear_locals();
+            },
+            |_| {
+                stack.exec(|frame| {
+                    frame.local_push(Value::Int(1));
+                    frame.local_push(Value::Int(2));
+                    frame.local_push(Value::Int(3));
+                    frame.local_push(Value::Int(4));
+                    frame.local_push(Value::Int(5));
+                });
+            }, 
+            criterion::BatchSize::SmallInput
         );
     });
 }
--- crates/wasm-test/src/runtime/stack.rs
+++ crates/wasm-test/src/runtime/stack.rs
@@ -36,11 +36,21 @@ impl Stack {
 
     #[inline]
     fn tombstone<'a>(&self, frame: &'a Frame<'_>) {
-        println!("TOMBSTONE ME!! {:?}", frame);
+        println!("Tombstoning frame: {}", frame.id);
+        println!("This frame has {} pointers to clean up.", frame.pointers.borrow().len());
+
+        frame.pointers
+            .borrow()
+            .iter()
+            .for_each(|ptr| self.locals.borrow_mut()[*ptr as usize] = None);
+
+        self.tombstoned_ptrs
+            .borrow_mut()
+            .append(&mut frame.pointers.borrow_mut());
     }
 
     #[inline]
-    pub fn local_push(&self, value: Value) -> i32 {
+    pub fn local_push<'a>(&self, value: Value) -> i32 {
         let ptr = self.locals.borrow().len() as i32;
 
         self.locals
@@ -67,16 +77,22 @@ impl Stack {
 pub struct Frame<'a> {
     id: u32,
     stack: &'a Stack,
-    ancestor: Option<&'a Frame<'a>>
+    ancestor: Option<&'a Frame<'a>>,
+    pointers: RefCell<Vec<i32>>
 }
 
 impl<'a> Frame<'_> {
     #[inline]
-    pub fn new(id: u32, stack: &'a Stack, ancestor: Option<&'a Frame<'a>>) -> Frame<'a> {
+    pub fn new(
+        id: u32, 
+        stack: &'a Stack, 
+        ancestor: Option<&'a Frame<'a>>
+    ) -> Frame<'a> {
         Frame {
             id,
             stack,
-            ancestor: ancestor.map(|frame| frame)
+            ancestor: ancestor.map(|frame| frame),
+            pointers: Default::default()
         }
     }
 
@@ -85,6 +101,13 @@ impl<'a> Frame<'_> {
         let frame = Frame::new(self.stack.next_frame_id(), self.stack, Some(self));
         func(frame);
         FrameResult {  }
+    }
+
+    #[inline]
+    pub fn local_push(&self, value: Value) -> i32 {
+        let ptr = self.stack.local_push(value);
+        self.pointers.borrow_mut().push(ptr);
+        ptr
     }
 }
 
@@ -108,10 +131,13 @@ mod test {
     fn test() {
         let stack = Stack::new();
         let result = stack.exec(|f1| {
+            f1.local_push(Value::Int(1));
+            f1.local_push(Value::Int(2));
+            f1.local_push(Value::Int(3));
             eprintln!("frame1: {:?}", f1);
             f1.frame(|f2| {
                 eprintln!("frame2: {:?}", f2);
-                stack.local_push(Value::Int(5));
+                f2.local_push(Value::Int(5));
             });
         });
 
